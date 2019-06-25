c19507eb8ae1254adffe172bc89a1e7cba74b7d5
as-segaf <ziyanassegaf20@gmail.com>
2019-06-25 19:59:45 +0700

    intial commit

---
+++ index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+
+	<body>
+		<?php 
+			session_start();
+			if (!isset($_SESSION['username'])) {
+				$_SESSION['message'] = "Anda belum login";
+				header('location:login.php');
+
+			} else{
+				$username = $_SESSION['username'];
+			}
+		 ?>
+
+		 <div class="content">
+		 	<p>Selamat datang <?php echo $username; ?>, Ini adalah halaman admin</p>
+		 	<a href="logout.php">Logout</a>
+		 </div>
+	</body>
+</html>
